3a5e033539d713dd73bc05d87cf73399fadcc865
Douglas Paz <eu@douglaspaz.com>
2019-03-26 11:44:49 -0300

    Add export to json

---
+++ src/export-to-json.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const codigosBancos = require('.');
+
+const jsonPath = process.argv[3] || './dist/codigos-bancos.json';
+
+codigosBancos()
+  .then(result => JSON.stringify(result, null, 2))
+  .then(content => fs.writeFileSync(jsonPath, content));
