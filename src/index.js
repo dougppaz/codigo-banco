c835240d054555d2634e7962298548ba977b95cf
Douglas Paz <eu@douglaspaz.com>
2019-03-26 11:44:34 -0300

    Add fisrt version

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+const cheerio = require('cheerio')
+
+module.exports = () => axios
+  .get('https://www.codigobanco.com/')
+  .then(response => response.data)
+  .then(cheerio.load)
+  .then($ => $('table tbody tr').map((i, elem) => {
+    const [code, name, site] = $(elem).find('td').map((i, elem) => $(elem).text()).get();
+    return {
+      code,
+      name,
+      site,
+    };
+  }))
+  .then(mapResult => mapResult.get());
