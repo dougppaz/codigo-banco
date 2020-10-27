56399b828fddbc72dd8adc24a2536b4610254a53
Douglas Paz <oi@douglaspaz.com>
2020-10-27 09:59:30 -0300

    change json dist output default name

--- src/export-to-json.js
+++ src/export-to-json.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const codigosBancos = require('.');
 
-const jsonPath = process.argv[3] || './dist/codigos-bancos.json';
+const jsonPath = process.argv[3] || './dist/codigo-banco.json';
 
 codigosBancos()
   .then(result => JSON.stringify(result, null, 2))
