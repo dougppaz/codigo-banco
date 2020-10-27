e5a7d68c84e5c50e747e88b28c9d02f5c317a3e7
Douglas Paz <oi@douglaspaz.com>
2020-10-27 10:25:24 -0300

    lint

--- src/export-to-json.js
+++ src/export-to-json.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const codigosBancos = require('.');
+const fs = require('fs')
+const codigosBancos = require('.')
 
-const jsonPath = process.argv[3] || './dist/codigo-banco.json';
+const jsonPath = process.argv[3] || './dist/codigo-banco.json'
 
 codigosBancos()
   .then(result => JSON.stringify(result, null, 2))
-  .then(content => fs.writeFileSync(jsonPath, content));
+  .then(content => fs.writeFileSync(jsonPath, content))
